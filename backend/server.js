09a5c668196b7c5f70ec772f06f063e5df7a2a16
ramasha-weddings <theramashaweddings@gmail.com>
2026-02-18 17:24:58 +0530

    fix: updated port for render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config()
-
 const app = require('./src/app')
-
 const connectToDB = require("./src/config/database")
 
+// Database connection
 connectToDB()
 
-app.listen(3000,(req,res)=>{
-    console.log("server is running on 3000");
-    
-})
+// --- YE BADLAV HAI ---
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
